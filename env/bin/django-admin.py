c1191ab7835a45d4f8d9179349ea1b495a6cd99a
ray24812 <ray24812@gmail.com>
2021-05-25 23:31:21 -0400

    wip

--- env/bin/django-admin.py
+++ env/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/home/freelance-projects/8-bit-apes-app/env/bin/python3
+#!/Users/home/Projects/crypto_apes/env/bin/python3.9
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
